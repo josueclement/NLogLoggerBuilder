64220937523df9ec97417357ba08862458e066d5
Josué Clément <josue.d.clement@gmail.com>
2025-02-01 21:51:32 +0100

    Add example for database target and SQLite

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -3,12 +3,26 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NLogLoggerBuilder;
 using NLog.Extensions.Logging;
+using NLog.Targets;
 
 namespace Tester;
 
 public static class Program
 {
     public static IServiceProvider? ServiceProvider { get; private set; }
+
+    private static void ConfigureServices()
+    {
+        var services = new ServiceCollection();
+        services.AddLogging(builder =>
+        {
+            builder.ClearProviders();
+            builder.AddNLog();
+        });
+        services.AddSingleton<Dummy>();
+        services.AddTransient<NLogLoggerProviderBuilder>();
+        ServiceProvider = services.BuildServiceProvider();
+    }
     
     public static void Main(string[] args)
     {
@@ -30,6 +44,32 @@ public static class Program
                         config.Layout = NLogLoggerProviderBuilder.DefaultLayout;
                         config.FileName = "Logs/tester.log";
                     })
+                .AddDatabaseTarget("myDatabaseTarget",
+                    config =>
+                    {
+                        // CREATE TABLE "LOGS" (
+                        //     "Id"	INTEGER,
+                        //     "Timestamp"	TEXT,
+                        //     "Level"	TEXT,
+                        //     "Message"	TEXT,
+                        //     "ThreadId"	INTEGER,
+                        //     "Callsite"	TEXT,
+                        //     "GCTotalMemory"	INTEGER,
+                        //     "Exception"	TEXT,
+                        //     PRIMARY KEY("Id" AUTOINCREMENT)
+                        // );
+                        config.KeepConnection = false;
+                        config.DBProvider = "System.Data.SQLite.SQLiteConnection, System.Data.SQLite";
+                        config.ConnectionString = "Data Source=C:\\Temp\\logs.db;Version=3;";
+                        config.CommandText = "INSERT INTO LOGS (Timestamp, Level, Message, ThreadId, Callsite, GCTotalMemory, Exception) VALUES (@Timestamp, @Level, @Message, @ThreadId, @Callsite, @GCTotalMemory, @Exception)";
+                        config.Parameters.Add(new DatabaseParameterInfo { Name = "@Timestamp", Layout = "${longdate}" });
+                        config.Parameters.Add(new DatabaseParameterInfo { Name = "@Level", Layout = "${level:uppercase=true}" });
+                        config.Parameters.Add(new DatabaseParameterInfo { Name = "@Message", Layout = "${message}" });
+                        config.Parameters.Add(new DatabaseParameterInfo { Name = "@ThreadId", Layout = "${threadid}" });
+                        config.Parameters.Add(new DatabaseParameterInfo { Name = "@Callsite", Layout = "${callsite:className=false:fileName=true:includeSourcePath=false:methodName=true}" });
+                        config.Parameters.Add(new DatabaseParameterInfo { Name = "@GCTotalMemory", Layout = "${gc:property=TotalMemory}" });
+                        config.Parameters.Add(new DatabaseParameterInfo { Name = "@Exception", Layout = "${exception:format=ToString}" });
+                    })
                 .BuildLogger("myLogger");
             var logger2 = ServiceProvider!.GetRequiredService<NLogLoggerProviderBuilder>()
                 .AddConsoleTarget("myConsoleTarget",
@@ -38,8 +78,16 @@ public static class Program
                         config.Layout = NLogLoggerProviderBuilder.DefaultLayout;
                     })
                 .BuildLogger("myLogger");
+
+            try
+            {
+                A();
+            }
+            catch (Exception ex)
+            {
+                logger.LogCritical(ex, "This is a critical message with value {value}", 12); 
+            }
             
-            logger.LogCritical("This is a critical message");
             logger2.LogCritical("This is a critical message");
             logger.LogError("This is an error message");
             logger.LogWarning("This is a warning message");
@@ -53,16 +101,7 @@ public static class Program
         }
     }
 
-    private static void ConfigureServices()
-    {
-        var services = new ServiceCollection();
-        services.AddLogging(builder =>
-        {
-            builder.ClearProviders();
-            builder.AddNLog();
-        });
-        services.AddSingleton<Dummy>();
-        services.AddTransient<NLogLoggerProviderBuilder>();
-        ServiceProvider = services.BuildServiceProvider();
-    }
+    private static void A() => B();
+    private static void B() => C();
+    private static void C() => throw new InvalidOperationException("Something failed...");
 }
